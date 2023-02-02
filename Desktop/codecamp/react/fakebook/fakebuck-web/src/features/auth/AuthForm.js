c73ea4be7436471d9a03f507cf8de1f40eeec040
Nuknoramon <nuknuk_ok@hotmail.com>
2023-02-02 16:36:14 +0700

    feature Register

---
+++ Desktop/codecamp/react/fakebook/fakebuck-web/src/features/auth/AuthForm.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Modal from "../../components/ui/Modal";
+import LoginForm from "./LoginForm";
+
+function AuthForm() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="border border-1 shadow p-3 bg-white mx-auto rounded-lg max-w-99">
+      <LoginForm />
+      <hr className="hr-sm" />
+      <div className="text-center tw-py-2.5">
+        <button
+          className="btn fw-bold btn-green rounded-md h-12"
+          type="button"
+          onClick={() => setIsOpen(true)}
+        >
+          Create New Account
+        </button>
+      </div>
+      <Modal title="test" open={isOpen} onClose={() => setIsOpen(false)}>
+        Modal Children body
+      </Modal>
+    </div>
+  );
+}
+
+export default AuthForm;
